491f7d0e936a53de7a50c32bd73403ad0b9aa8b4
hakunamatata0314 <li.jiayao@husky.neu.edu>
2020-04-11 23:54:19 -0700

    add Leetcode problem descriptions

--- 559MaximumDepthofN-aryTree.py
+++ 559MaximumDepthofN-aryTree.py
@@ -1,9 +1,22 @@
 '''
     Jiayao Li
     CS 5001
-    Hw 9
-    April 11st, 2020
-    Programming #3 (Leetcode 559)  Maximum Depth of N-ary Tree
+(Leetcode 559)  Maximum Depth of N-ary Tree
+
+Given a n-ary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path 
+from the root node down to the farthest leaf node.
+Nary-Tree input serialization is represented in their level order traversal, 
+each group of children is separated by the null value (See examples).
+
+Example 1:
+      1
+    / | \
+   3  2  4
+  / \ 
+ 5   6
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
 '''
 
 """
